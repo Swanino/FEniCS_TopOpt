cbee3c0d7027c26c910319a17d6589210ea48a1e
Hayoung Chung <hychung@unist.ac.kr>
2023-04-21 15:35:24 +0900

    last

--- topopt/elasticity.py
+++ topopt/elasticity.py
@@ -8,19 +8,24 @@ from matplotlib import pyplot as plt
 
 # OPTIMIZATION PARAMETERS -------------------------------------------------
 class OptimPars:
-    def __init__(self, nelx=32, nely=32, nelz=32, pen=3., rmin=3., ft=1, max_iter=100, max_load=1.0, volfrac=0.5):
-        self.nelx = nelx
-        self.nely = nely
-        self.nelz = nelz
+    def __init__(self, pen=3., rmin=3., ft=1, max_iter=100, volfrac=0.5)->None:
         self.pen = pen
         self.rmin = rmin
         self.ft = ft
         self.max_iter = max_iter
-        self.max_load = max_load
         self.volfrac = volfrac
     
-    def __str__(self):
-        return f"OptimPars(nelx={self.nelx}, nely={self.nely}, nelz={self.nelz}, pen={self.pen}, rmin={self.rmin}, ft={self.ft}, max_iter={self.max_iter}, max_load={self.max_load}, volfrac={self.volfrac})"
+    def __str__(self) -> str:
+        return f"OptimPars(pen={self.pen}, rmin={self.rmin}, ft={self.ft}, max_iter={self.max_iter}, volfrac={self.volfrac})"
 
 # LINEAR ELASTICITY PROBLEM -------------------------------------------------
+class ElasticPars:
+    def __init__(self, nelx=32, nely=32, nelz=32, E=1.0, mu=1.) -> None:
+        self.nelx = nelx
+        self.nely = nely
+        self.nelz = nelz
+        self.E = E
+        self.mu = mu
 
+    def __str__(self) -> str:
+        return f"ElasticPars(nelx={self.nelx}, nely={self.nely}, nelz={self.nelz}, E={self.E}, mu={self.mu})"
